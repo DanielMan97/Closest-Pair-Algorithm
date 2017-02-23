0881da493ee39d92a2a73c58c6761fa546f9781d
DanielMan97 <daniel.mx@hotmail.co.uk>
2017-02-23 19:50:39 +0000

    finished need to clean up code

--- src/closestPair/Test.java
+++ src/closestPair/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	public static void main(String[] argv) throws InvalidNumberOfTestsException, UnwritablePointSetException {
       //   ClosestPairToolkit.getRuntimes(10, 250,"closestPairTimes.txt");
      //   ClosestPairToolkit.getRatios(10, 20,2,"Ratios.txt");
-	     ClosestPairToolkit.plotRuntimes( 0.6721,  0.6242, "closestPairTimes.txt");
+	  //   ClosestPairToolkit.plotRuntimes( 0.6721,  0.6242, "closestPairTimes.txt");
 
 	}
 }
